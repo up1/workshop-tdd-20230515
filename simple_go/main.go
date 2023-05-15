05c2ab39c2782e452fe56a5bfd6aaf34414f0658
up1 <somkiat.p@gmail.com>
2023-05-15 16:27:37 +0700

    Update go api

--- simple_go/main.go
+++ simple_go/main.go
@@ -6,12 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	r.Run(":8080")
 }
