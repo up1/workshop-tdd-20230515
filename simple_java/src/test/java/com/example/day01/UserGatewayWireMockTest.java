aedb895e628f4d71cc62bff47c2bd9d2ec6e8538
up1 <somkiat.p@gmail.com>
2023-05-16 13:43:46 +0700

    Add wiremock + stubFor

--- simple_java/src/test/java/com/example/day01/UserGatewayWireMockTest.java
+++ simple_java/src/test/java/com/example/day01/UserGatewayWireMockTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,6 +18,12 @@ public class UserGatewayWireMockTest {
 
     @Test
     public void successWithUserId() {
+        // Arrange
+        stubFor(get(urlEqualTo("/users/1")).willReturn(aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withStatus(200)
+                .withBody("{\"name\": \"Leanne Graham\", \"email\": \"Sincere@april.biz\"}")));
+
         // Act
         UserResponse result = userGateway.getById(1);
         // Assert
